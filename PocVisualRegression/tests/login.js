69c04ef45a094d13adecb5eea80036e3e36f500e
krazevedo <kaio.azevedo85@gmail.com>
2018-03-20 10:07:15 -0300

    alterando user e login para usar variaveis globais

--- PocVisualRegression/tests/login.js
+++ PocVisualRegression/tests/login.js
@@ -6,8 +6,8 @@ module.exports = {
         .waitForElementVisible('body', 1000)
         .assert.elementPresent('h2','Welcome')
         .assert.visible('input[type=text]')
-        .setValue('input[type=text]', '')
-        .setValue('input[type=password]', '')
+        .setValue('input[type=text]', 'process.env.GOAL_USER')
+        .setValue('input[type=password]', 'process.env.GOAL_PASS')
         .assert.visualRegression() 
         .end()       
     }
